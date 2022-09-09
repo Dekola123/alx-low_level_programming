514cf86719ee526615def2dfe8f761e66d62c76d
Samuel Adekola <samueltobi004@gmail.com>
2022-09-09 09:45:30 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low, e, q;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'e' && 'q')
+			putchar(low);
+	}
+	printf("\n");
+	
+	return (0);
+}
