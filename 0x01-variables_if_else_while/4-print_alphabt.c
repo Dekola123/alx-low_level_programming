70dc21829efd1e529f0972d0fa400658d77850d7
Samuel Adekola <samueltobi004@gmail.com>
2022-09-09 09:57:57 -0700

    printing alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,10 +15,10 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != e && low !=q)
+		if (low != e && low != q)
 			putchar(low);
 	}
-	printf("\n");
+	printf ("\n");
 	
 	return (0);
 }
