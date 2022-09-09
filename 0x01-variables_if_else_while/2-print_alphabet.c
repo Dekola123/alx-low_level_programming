fd45f80f76ff8296ee621a23d09f4ad090af506b
Samuel Adekola <samueltobi004@gmail.com>
2022-09-09 09:11:22 -0700

    printing on screen

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,6 +12,5 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	
 	return (0);
 }
