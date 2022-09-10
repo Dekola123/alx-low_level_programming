af8b5d3caac9bca0af958f0c423b21d2d2314e10
Samuel Adekola <samueltobi004@gmail.com>
2022-09-10 01:14:46 -0700

    c programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	int i;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		if (i <= '9');
+		{
+			d = d && i;
+			i++;
+		
+			putchar(d);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
