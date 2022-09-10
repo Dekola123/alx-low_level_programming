6bea3d049a529940699de372a8132ae7bcd79792
Samuel Adekola <samueltobi004@gmail.com>
2022-09-10 09:16:27 -0700

    starting to code in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(d));
-	printf("Size of an int: %u byte(s)\n", (unsigned)sizeof(a));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a foat: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a foat: %lu bytes(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
