bc73dee1deef50e5d14b3abbf2a1f31e0bb10a38
Samuel Adekola <samueltobi004@gmail.com>
2022-09-15 06:57:23 -0700

    functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, n;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		while (j <= 14)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
-			j++
+			if (j >= 10)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
-		i++;
 	}
 }
-
