2fd135aececa7151ea6398899f74a227e5be119a
Samuel Adekola <samueltobi004@gmail.com>
2022-09-16 03:46:44 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - print and integer
+ * @n: integer to print
+ * Return: nothing to return
+ */
+void print_number(int n)
+{
+	_putchar(n);
+	_putchar('\n');
+}
