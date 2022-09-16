f9b5e3316180433de737d60ebad0c5ca243a699f
Samuel Adekola <samueltobi004@gmail.com>
2022-09-16 04:30:00 -0700

    functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,12 @@ void print_number(int n)
 	unsigned int x;
 
 	x = n;
-
-	if (n < 0);
+	if (n < 0)
 	{
 		_putchar(45);
-		_putchar(x);
 		x = -n;
 	}
-	if (n >= 0);
-	{
-		_putchar(x);
-		x = n;
-	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
