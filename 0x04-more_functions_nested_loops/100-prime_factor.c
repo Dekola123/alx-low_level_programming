80b765a47274e83d5d37d5b7d17e9515cd6d2a50
Samuel Adekola <samueltobi004@gmail.com>
2022-09-16 03:04:40 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -  printing prime factors of a number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int n, pf;
+
+	n = 612852475143;
+
+	for (pf = 2; pf <= n; pf++)
+	{
+		if (n % pf == 0)
+		{
+			n = n / pf;
+		}
+	}
+	printf("%ld\n", pf);
+}
