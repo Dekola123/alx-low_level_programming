8fa40a857f99d80940bf3e1cfe5fd79ea026abe7
Samuel Adekola <samueltobi004@gmail.com>
2022-09-19 08:28:07 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int strlen(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char);
 void print_rev(char *s);
