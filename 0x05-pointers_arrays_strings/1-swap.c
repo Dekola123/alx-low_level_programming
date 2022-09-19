71b22fe08a83fe2b26efefe1c8ffd5d246b46857
Samuel Adekola <samueltobi004@gmail.com>
2022-09-19 05:20:58 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap - swap the value of two int
+ * @a: first int
+ * @b: second int
+ *
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int k;
+	int l;
+
+	k = *b;
+	l = *a;
+}
+
+
