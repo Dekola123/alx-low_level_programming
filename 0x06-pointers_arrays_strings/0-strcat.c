17c18c2bf5c7a9521ab409178783c8456b4d04da
Samuel Adekola <samueltobi004@gmail.com>
2022-09-21 08:01:48 -0700

    more on pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,12 +5,12 @@
 * @src: second string
 * Return: a pointer to a string.
 */
-char *_strcat(char *dest, char *src)
+char _strcat(char *dest, char *src)
 {
 	int i, j;
 
-	int i = 0;
-	int j = 0;
+	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 	{
