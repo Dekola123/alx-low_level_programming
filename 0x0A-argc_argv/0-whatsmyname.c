b5a223728c2ad81cc9cace0f010e4f2e5ad01ca1
Samuel Adekola <samueltobi004@gmail.com>
2022-09-30 06:33:16 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints functions name
+ * @argc: count of arguments
+ * @argv: arrays of arg
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
